af8438ad3141c4a43cffe825121e82a44367e2aa
Yujin Im <jina0605@gmail.com>
2023-08-17 05:22:36 +0900

    Add test to Makefile and Implement compare function
    Probably not work for multiple test cases

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ CXX := g++
 CXXFLAGS := -std=c++11 -O3 -w -DNDEBUG
 CPPFLAGS := -Iinclude
 EXTRA_FLAGS := -DDYNAMIC_ORDERING -DFILTERING_BY_NEIGHBOR_SAFETY -DLEAF_ADAPTIVE_MATCHING -DN_OPTIMIZATION -DPRUNING_BY_EQUIVALENCE_SETS
+TEST_LIBS := -lgtest -lpthread
 
 SRC := src
 OBJ := obj
@@ -17,7 +18,7 @@ all: $(VEQ_S)
 
 $(VEQ_S): $(SRC)/main.cpp
 	$(CXX) $(CXXFLAGS) $(CPPFLAGS) $(EXTRA_FLAGS) -c $(SRC)/main.cpp -o $(SRC)/main.o
-	$(CXX) $(CXXFLAGS) $(SRC)/main.o -o $@
+	$(CXX) $(CXXFLAGS) $(SRC)/main.o $(TEST_LIBS) -o $@
 
 $(SRC)/%.o: $(SRC)/%.cpp
 	$(CXX) $(CXXFLAGS) $(CPPFLAGS) -c $< -o $@
@@ -28,3 +29,6 @@ clean:
 
 run: $(VEQ_S)
 	./$(VEQ_S) -dg graph/data/COLLAB.gfu -qg graph/query/COLLAB/randomwalk/8/q30.gfu
+
+test: $(VEQ_S)
+	./$(VEQ_S) -test
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 // Adaptive matching order with new extendable candidate definition
+#include <gtest/gtest.h>
 #include "DAF.h"
 
 void writeAnswer(string& answerFile) {
@@ -72,6 +73,48 @@ inline void ProcessQuery() {
   }
 }
 
+void compare(string dataGraphFile, const int queryCnt, string queryGraphFiles[], string resultFiles[]) {
+  ReadGFUFormat(dataGraphFile, dataGraph);
+
+  ProcessDataGraphs();
+  AllocateForDataGraph();
+
+  for (int i = 0; i < queryCnt; i++) {
+    ReadGFUFormat(queryGraphFiles[i], queryGraph);
+    ProcessQuery();
+
+    ifstream inFile(resultFiles[i]);
+    if (!inFile) {
+      cout << "Failed to open a data result file " << resultFiles[i] << endl;
+      exit(1);
+    }
+
+    vector<int> expectedResult;
+    while (!inFile.eof()) {
+      int num;
+      inFile >> num;
+      expectedResult.push_back(num);
+    }
+
+    EXPECT_EQ(recursiveCallCount, expectedResult[0]);
+    EXPECT_EQ(nCandidate, expectedResult[1]);
+    EXPECT_EQ(nMatch, expectedResult[2]);
+    vector<int>::iterator expectedResultIter = expectedResult.begin() + 3;
+    for (int g = 0; g < nGraph; ++g) {
+      bool inExpectedResult = expectedResultIter != expectedResult.end() && *expectedResultIter == g;
+      EXPECT_EQ(answer[g], inExpectedResult);
+      if(inExpectedResult) expectedResultIter++;
+    }
+  }
+}
+
+TEST(compare_test, compare_test_1) {
+  string queryGraphFiles[1] = {"graph/query/IMDB-MULTI/bfs/8/q0.gfu"};
+  string resultFiles[1] = {"testdata/IMDB-MULTI/bfs/8/q0.txt"};
+
+  compare("graph/data/IMDB-MULTI.gfu", 1, queryGraphFiles, resultFiles);
+}
+
 // Subgraph query
 // Usage: ./Program -d[dataFormat] [dataFile] -q[queryFormat] [queryFile] -o
 // [answerFile]
@@ -123,6 +166,9 @@ int main(int argc, char* argv[]) {
         case 'o':
           answerFile = argv[i + 1];
           break;
+        case 't':
+          ::testing::InitGoogleTest(&argc, argv);
+          return RUN_ALL_TESTS();
       }
     }
   }
